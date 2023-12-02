eba6c1d6ff91e6ca78a82c16f7b0c5a52dc26a46
Vencislav <vencislav67@hotmail.com>
2023-12-02 12:33:16 +0200

    9

---
+++ 9_FruitsVegetavbles/Program.cs
@@ -0,0 +1,21 @@
+﻿string s = Console.ReadLine();
+switch (s)
+{
+    case "banana":
+    case "apple":
+    case "kiwi":
+    case "cherry":
+    case "lemon":
+    case "grapes":
+        Console.WriteLine("fruit");
+        break;
+    case "tomato":
+    case "cucumber":
+    case "pepper":
+    case "carrot":
+        Console.WriteLine("vegetable");
+        break;
+    default:
+        Console.WriteLine("unknown");
+        break;
+}
