13e3d65b8a8b9768fe35ecac7229a615e8362a41
Vencislav <vencislav67@hotmail.com>
2023-12-02 12:30:51 +0200

    7

---
+++ 7_WorkingHours/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = int.Parse(Console.ReadLine());
+string b = Console.ReadLine();
+if (b == "Sunday")
+{
+    Console.WriteLine("closed");
+}
+else if (a > 9 && a < 19)
+{
+    Console.WriteLine("open");
+}
+else
+{
+    Console.WriteLine("closed");
+}
