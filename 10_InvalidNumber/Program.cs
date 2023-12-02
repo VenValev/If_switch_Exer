2d01992f84cc8a626e4e81d8a72eaf4e5eb4ef37
Vencislav <vencislav67@hotmail.com>
2023-12-02 12:34:36 +0200

    10

---
+++ 10_InvalidNumber/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = int.Parse(Console.ReadLine());
+if ((a < 100 || a > 200) && a != 0)
+{
+    Console.WriteLine("invalid");
+}
+//Console.WriteLine((a<100 || a>200) && a!=0 ? $"invalid" : $"" ); Същото като горното т.н. Тернарен оператор ternari
