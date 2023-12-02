121bcb53a38fbe460b76438f2a414946d679bb37
Vencislav <vencislav67@hotmail.com>
2023-12-02 12:27:00 +0200

    4

---
+++ 4_AgeSex/Program.cs
@@ -0,0 +1,24 @@
+﻿double age = double.Parse(Console.ReadLine());
+string sex = Console.ReadLine();
+if (sex == "f")
+{
+    if (age < 16)
+    {
+        Console.WriteLine("Miss");
+    }
+    else
+    {
+        Console.WriteLine("Ms.");
+    }
+}
+else
+{
+    if (age < 16)
+    {
+        Console.WriteLine("Master");
+    }
+    else
+    {
+        Console.WriteLine("Mr.");
+    }
+}
